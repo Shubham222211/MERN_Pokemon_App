4845800af3d59ff5860e22abb610e11d975f4445
Shubham222211 <123923464+Shubham222211@users.noreply.github.com>
2024-12-20 22:28:34 +0530

    Pokemon App

---
+++ PokemonRouter.js
@@ -0,0 +1,40 @@
+
+const express = require('express');
+const notesRouter=express.Router()
+
+const pokemonModel = require('./PokemonSchema.js');
+
+const pokemonRouter = express.Router();
+
+
+pokemonRouter.post('/create', async (req, res) => {
+  try {
+    const { name, sprite, types } = req.body;
+    if (!name || !types || types.length === 0) {
+      return res.status(400).send({ message: 'Name and types are required' });
+    }
+
+    
+    if (!Array.isArray(types)) {
+      return res.status(400).send({ message: 'Types should be an array of strings' });
+    }
+
+    const newPokemon = new pokemonModel({
+      name,
+      sprite,
+      types
+    });
+    const savedPokemon = await newPokemon.save();
+    res.status(201).send({
+      name: savedPokemon.name,
+      sprite: savedPokemon.sprite,
+      types: savedPokemon.types
+    });
+  } catch (error) {
+    res.status(500).send({ message: 'Error creating Pokemon', error });
+  }
+});
+
+
+
+module.exports=pokemonRouter
